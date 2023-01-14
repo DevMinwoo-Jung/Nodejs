1a7c43ec4c4e1df220b7f6a58155606378d32304
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-01-14 20:30:00 +0900

    nn

--- category/app.js
+++ category/app.js
@@ -1,11 +1,27 @@
-var fs = require('fs'),
-    path = require('path'),    
-    filePath = path.join(__dirname, '../test/');
+var fs = require('fs');
+var path = require('path');
+var filePath = path.join(__dirname, '../test/');
 
-fs.readdir(filePath, {encoding: 'utf-8'}, function(err,data){
-    if (!err) {
-        console.log('received data: ' + data);
-    } else {
-        console.log(err);
-    }
-});
+let asd = getAllFiles(filePath);
+
+console.log(asd);
+
+function getAllFiles(filePath) {
+  let fileArray = [];
+  fs.readdir(filePath, {encoding: 'utf-8'}, function(err,data){
+      if (!err) {
+          // console.log(data);
+          data.map((element) => fileArray.push(element));
+          fileArray = data;
+      } else {
+          console.log(err);
+      }
+  });
+  console.log(`fileArray: ${fileArray}`)
+  return fileArray;
+};
+
+function setFilterFileExtenstion(files, type) {
+  let returnArr = [];
+  
+}

dac7c16af1b0e737e25ec95d2c8b3f2eb6559517
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-01-18 20:30:00 +0900

    Thank you stackOverfolw

--- category/app.js
+++ category/app.js
@@ -6,19 +6,9 @@ let asd = getAllFiles(filePath);
 
 console.log(asd);
 
-function getAllFiles(filePath) {
-  let fileArray = [];
-  fs.readdir(filePath, {encoding: 'utf-8'}, function(err,data){
-      if (!err) {
-          // console.log(data);
-          data.map((element) => fileArray.push(element));
-          fileArray = data;
-      } else {
-          console.log(err);
-      }
-  });
-  console.log(`fileArray: ${fileArray}`)
-  return fileArray;
+function getAllFiles(path) {
+  let file = fs.readdirSync(path);
+  return file;
 };
 
 function setFilterFileExtenstion(files, type) {

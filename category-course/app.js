bfb712056189b9a26b4fe95b4273688c005f4d87
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-01-26 20:30:00 +0900

    asdasd

--- category-course/app.js
+++ category-course/app.js
@@ -2,16 +2,24 @@ const path = require('path');
 const os = require('os');
 const fs = require('fs');
 // get folders user want
-// make video, captured... folders
-// move files to matched folders
 
 // get params
 const folder = process.argv[2];
-const workingDir = path.join(os.homedir(), 'Pictures', folder);
+const workingDir = path.join(__dirname, 'Pictures', folder);
+
+console.log(workingDir);
 
 if(!folder || fs.existsSync(workingDir)) {
   console.error('사진이나 폴더 이름이 없습니다.');
 }
 
+// make video, captured... folders
+const videoDir = path.join(workingDir, 'video');
+const capturedDir = path.join(workingDir, 'captured');
+const duplicatedDir = path.join(workingDir, 'duplicated');
 
-console.log(workingDir);
+fs.existsSync(videoDir) && fs.mkdirSync(videoDir);
+fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);
+fs.existsSync(duplicatedDir) && fs.mkdirSync(duplicatedDir);
+
+// move files to matched folders

4de1fb1dc5ec36568aeea7a5e8f927bc060209c8
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-07-04 21:21:43 +0900

    get sample

--- expressJS/app.js
+++ expressJS/app.js
@@ -5,8 +5,16 @@ const app = express();
 // IP
 // Port
 
-app.get('/', (req, res, next) => {
-  console.log('get');
+app.get('/sky/:id', (req, res, next) => {
+  // console.log(req);
+  // console.log(req.header);
+  console.log(req.params);
+  // { id: 'minwoo' }
+  // 이렇게 접근 가능
+  console.log(req.params.id);
+  console.log(req.query);
+  //{ keyword: 'roWjsek' }
+  console.log(req.query.keyword);
   res.send('안녕 express!')
 });
 

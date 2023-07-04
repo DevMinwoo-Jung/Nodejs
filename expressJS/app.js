0a5b78ea12694e569e47763f5ff3f5d904ff5df2
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-07-04 13:14:18 +0900

    res sample

--- expressJS/app.js
+++ expressJS/app.js
@@ -5,17 +5,24 @@ const app = express();
 // IP
 // Port
 
-app.get('/sky/:id', (req, res, next) => {
-  // console.log(req);
-  // console.log(req.header);
-  console.log(req.params);
-  // { id: 'minwoo' }
-  // 이렇게 접근 가능
-  console.log(req.params.id);
-  console.log(req.query);
-  //{ keyword: 'roWjsek' }
-  console.log(req.query.keyword);
-  res.send('안녕 express!')
+// app.get('/sky/:id', (req, res, next) => {
+//   // console.log(req);
+//   // console.log(req.header);
+//   console.log(req.params);
+//   // { id: 'minwoo' }
+//   // 이렇게 접근 가능
+//   console.log(req.params.id);
+//   console.log(req.query);
+//   //{ keyword: 'roWjsek' }
+//   console.log(req.query.keyword);
+//   res.send('안녕 express!')
+// });
+
+app.get('/', (req, res, next) => {
+
+  res.setHeader('key', 'value')
+  res.status(201).send('created')
+  res.json({name: 'minwoo'})
 });
 
 app.listen(8080);

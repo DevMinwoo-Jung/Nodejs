e73e53011fff948d49822e2c34a1b21daf0043f6
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-07-05 20:54:23 +0900

    middle ware?

--- expressJS/app.js
+++ expressJS/app.js
@@ -18,11 +18,53 @@ const app = express();
 //   res.send('안녕 express!')
 // });
 
-app.get('/', (req, res, next) => {
+// app.get('/', (req, res, next) => {
+//   console.log('first')
+// });
 
-  res.setHeader('key', 'value')
-  res.status(201).send('created')
-  res.json({name: 'minwoo'})
-});
+// app.get('/', (req, res, next) => {
+//   console.log('second')
+// });
+
+// // 위처럼 함녀 first만 나온다
+
+// app.get('/', 
+//   (req, res, next) => {
+//     console.log('first');
+//     next();
+//     // 이걸 하면 밑에를 갔다가
+//   },
+//   (req, res, next) => {
+//     console.log('second');
+//     next();
+//     // 여기서도 이걸 써줘야 끝남
+//   }
+// );
+
+app.get('/', 
+  (req, res, next) => {
+    console.log('first');
+    next('route');
+    // 이걸 하면 밑에를 갔다가
+  },
+  (req, res, next) => {
+    console.log('second');
+    next();
+    // 위에서 route해서 다음꺼 탐
+  }
+);
+
+app.use((req, res, next) => {
+  res.status(400).send('Not available!')
+})
+
+app.get('/', (req, res, next) => {
+  console.log('thrid')
+})
+
+app.use((error, req, res, next)=> {
+  console.error(error);
+  res.status(500).send('관리자에게 문의하십시오');
+})
 
 app.listen(8080);

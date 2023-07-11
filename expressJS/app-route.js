09bca7c695db2dc3672e2f3c525bd6e2d674cec5
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-07-11 22:23:45 +0900

    cors

--- expressJS/app-route.js
+++ expressJS/app-route.js
@@ -1,11 +1,23 @@
 import express from "express";
-import postRouter from './router/posts'
-import userRouter from './router/user'
+import cors from 'cors'
 const app = express();
 
-app.use(express.json());
+app.use(cors({
+  origin: ['http://127.0.0.1:5000'],
+  optionsSuccessStatus: 200,
+  credentials: true, // Access-Content
+}));
 
-app.use('/posts', postRouter)
-app.use('/users', userRouter)
+app.use(express.json()); // REST API -> Body
+
+app.use(express.urlencoded({extended: false})); // HTML Form -> Body
+// app.use(express.static('public'))
+
+// app.use('/posts', postRouter)
+// app.use('/users', userRouter)
+
+app.get('/', (req, res, next)=> {
+  console.log('GET!')
+})
 
 app.listen(8080);
--- expressJS/post.js
+++ expressJS/post.js
@@ -1,10 +1,23 @@
 import express from "express";
-const post = express();
+const app = express();
 
-post.use(express.json());
+app.use(express.json());
 
-post.post('/', (req, res, next) => {
+app.post('/', (req, res, next) => {
   console.log(req.body);
+})
+
+app.get('/file', (req, res) => {
+  fs.readFile('/file1.tx', (err, data) => {
+    if(err) {
+      res.sendStatus(404);
+    }
+  })
+})
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).json({message: '무언가가 잘못됐슈'})
 })
 
 post.listen(8080);
